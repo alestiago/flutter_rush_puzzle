5da89e5ed139d7edec719570f1f7844200f0c565
alestiago <dev@alestiago.com>
2022-03-01 10:29:37 +0000

    feat: implement API for side colors

--- packages/z_cubic_text/lib/src/z_cubic_text_style.dart
+++ packages/z_cubic_text/lib/src/z_cubic_text_style.dart
@@ -4,8 +4,20 @@ class ZCubicTextStyle {
   const ZCubicTextStyle({
     this.fontSize = 10.0,
     this.color = const Color(0xFF000000),
+    this.frontColor,
+    this.rearColor,
+    this.leftColor,
+    this.rightColor,
+    this.topColor,
+    this.bottomColor,
   });
 
   final double fontSize;
   final Color color;
+  final Color? frontColor;
+  final Color? rearColor;
+  final Color? leftColor;
+  final Color? rightColor;
+  final Color? topColor;
+  final Color? bottomColor;
 }
